2e1f95c3afd92eb8d60b55782834f833165ee878
Gerard-Alexnder <geraldalmanzar07@gmail.com>
2026-09-13 23:48:02 -0400

    tarea: Agregar datos iniciales de gastos

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from practica-gastos-negocio!")
+gastos_negocio = [
+
+    {"descripcion" : "Renta del local", "monto": 15000.00, "categoria" : "Renta"},
+    {"descripcion" : "Compra de insumos", "monto": 3500.00, "categoria" : "Insumos"},
+    {"descripcion" : "Publicidad en redes", "monto": 1200.00, "categoria" : "Marketing"},
+
+]
+
+#Mostrar los Gastos del Negocio
+# print("Gastos del negocio:")
+# for gasto in gastos_negocio:
+#     print(f"Descripcion: {gasto['descripcion']}, Monto: {gasto['monto']}, Categoria: {gasto['categoria']}")
 
 
-if __name__ == "__main__":
-    main()
